00b134983a7f7d2bf2bc7d32a8006d305ced789b
munyolec <munyolec@gmail.com>
2020-07-09 13:48:49 +0300

    article preview frontend mentor challenge

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', (event) =>{
+    let shareBtn=document.querySelector(".sharebtn");
+    let sharePalette=document.querySelector(".palette");
+
+    shareBtn.addEventListener("click", function(){
+        if(sharePalette.classList.contains("hidden")){
+            sharePalette.classList.remove("hidden");
+            shareBtn.style.backgroundColor="hsl(214,17%,51%)"
+            shareBtn.style.color="white";
+        }
+        else{
+            sharePalette.classList.add("hidden");
+            shareBtn.style.backgroundColor="hsl(214,17%,51%";
+        }
+    });
+});
+
